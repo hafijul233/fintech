68f8842a9bc4ddef8d35286280de1fde2e1975f4
Hafijul Islam <hafijul233@gmail.com>
2022-11-12 01:34:30 +0600

    dashboard uri label fixed

--- app/Nova/Dashboards/MainDashboard.php
+++ app/Nova/Dashboards/MainDashboard.php
@@ -2,12 +2,38 @@
 
 namespace App\Nova\Dashboards;
 
+use Illuminate\Support\Str;
 use Laravel\Nova\Cards\Help;
 use Laravel\Nova\Dashboards\Main as Dashboard;
 
 class MainDashboard extends Dashboard
 {
-    public $name = 'Dashboard';
+    /**
+     * The displayable name of the dashboard.
+     *
+     * @return string
+     */
+    public function name()
+    {
+        return 'Dashboard';
+    }
+
+    /**
+     * Determines whether Nova should show a refresh button.
+     *
+     * @var bool
+     */
+    public $showRefreshButton = true;
+
+    /**
+     * Get the URI key of the dashboard.
+     *
+     * @return string
+     */
+    public function uriKey()
+    {
+        return 'main';
+    }
 
     /**
      * Get the cards for the dashboard.
