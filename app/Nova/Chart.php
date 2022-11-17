c5eb6d3267f2b106b831a1299dc8749287f6f25e
Hafijul Islam <hafijul233@gmail.com>
2022-11-17 17:10:54 +0600

    sort and filter field added

--- app/Nova/Chart.php
+++ app/Nova/Chart.php
@@ -69,7 +69,7 @@ class Chart extends Resource
     /**
      * Get the fields displayed by the resource.
      *
-     * @param  NovaRequest  $request
+     * @param NovaRequest $request
      * @return array
      */
     public function fields(NovaRequest $request)
@@ -89,6 +89,8 @@ class Chart extends Resource
 
             Boolean::make('Enabled')
                 ->nullable()
+                ->sortable()
+                ->filterable()
                 ->default(true),
 
             DateTime::make('Created', 'created_at')
@@ -104,7 +106,7 @@ class Chart extends Resource
     /**
      * Get the cards available for the request.
      *
-     * @param  NovaRequest  $request
+     * @param NovaRequest $request
      * @return array
      */
     public function cards(NovaRequest $request)
@@ -117,7 +119,7 @@ class Chart extends Resource
     /**
      * Get the filters available for the resource.
      *
-     * @param  NovaRequest  $request
+     * @param NovaRequest $request
      * @return array
      */
     public function filters(NovaRequest $request)
@@ -128,7 +130,7 @@ class Chart extends Resource
     /**
      * Get the lenses available for the resource.
      *
-     * @param  NovaRequest  $request
+     * @param NovaRequest $request
      * @return array
      */
     public function lenses(NovaRequest $request)
@@ -139,7 +141,7 @@ class Chart extends Resource
     /**
      * Get the actions available for the resource.
      *
-     * @param  NovaRequest  $request
+     * @param NovaRequest $request
      * @return array
      */
     public function actions(NovaRequest $request)
