2e0e0f50aa4b7f87a9355f5ff02c6d8536f3a653
hafijul233 <hafijul233@users.noreply.github.com>
2022-11-19 17:17:35 +0000

    Fix styling

--- app/Nova/Metrics/Report/RevenueTableMetric.php
+++ app/Nova/Metrics/Report/RevenueTableMetric.php
@@ -18,7 +18,7 @@ class RevenueTableMetric extends Table
     /**
      * Calculate the value of the metric.
      *
-     * @param NovaRequest $request
+     * @param  NovaRequest  $request
      * @return mixed
      */
     public function calculate(NovaRequest $request)
